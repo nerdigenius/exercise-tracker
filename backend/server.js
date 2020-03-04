01d5c21dd0c8550a955153aac583e442f70aa72d
nerdgenius <Ashiq.racer@gmail.com>
2020-03-04 22:04:48 +0600

    ..

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =process.env.ATLAS_URI;
+
+mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex:true});
+
+app.listen(port,()=>{
+    console.log(`Server started at port ${port}`);
+});
