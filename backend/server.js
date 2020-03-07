5decdf5dfc2e3f20a298ad6a305a90dd606c8abf
nerdgenius <Ashiq.racer@gmail.com>
2020-03-07 14:55:51 +0600

    backend first phase done

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,32 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+
 app.use(cors());
 app.use(express.json());
 
 const uri =process.env.ATLAS_URI;
 
-mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex:true});
+mongoose.connect(uri,{useNewUrlParser: true, useCreateIndex:true,useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+connection.once('open',()=>{
+    console.log('mongoDB connnected successfully');
+});
+
+const exerciseRoute = require('./routes/exercise');
+const userRoute = require('./routes/users');
+
+app.use('/exercise',exerciseRoute);
+app.use('/users',userRoute);
 
 app.listen(port,()=>{
     console.log(`Server started at port ${port}`);
