d6254d4874a49bfa9bcfa09d31ebedc4e7fc6089
nerdgenius <Ashiq.racer@gmail.com>
2020-03-18 14:28:13 +0600

    started connecting backend

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export default class EditExercise extends Component {
+    render(){
+        return(
+               <h1>edit exer works</h1>
+        )
+    }
+}
---
+++ src/components/exercise-list.component.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export default class ExerciseList extends Component {
+    render(){
+        return(
+               <h1>List WOrdks</h1>
+        )
+    }
+}
